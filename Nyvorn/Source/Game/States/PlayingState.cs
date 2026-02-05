2d4d9b95d479d24e1e779e0c1af27ac680316268
Israel <israel.oliversw@gmail.com>
2026-02-05 11:36:57 -0300

    Adicionado pulo e gravidade.

--- Nyvorn/Source/Game/States/PlayingState.cs
+++ Nyvorn/Source/Game/States/PlayingState.cs
@@ -7,37 +7,89 @@ namespace Nyvorn.Source.Game.States
 {
     public class PlayingState
     {
-        private Texture2D _red;
+        GraphicsDevice graphicsDevice;
 
-        public Vector2 Position;
-        private float speed = 150f;
-        //private float gravity = 500f; // Gravidade
+        private Texture2D _red;
+        public Vector2 position;
+        private Vector2 velocity;
+
+        private const float moveSpeed = 150f; //velocidade
+        private float gravity = 800; // Gravidade
+
+        private bool isGrounded;
 
         public PlayingState(GraphicsDevice graphicsDevice, ContentManager content)
         {
-            
+            this.graphicsDevice = graphicsDevice;
+
             _red = new Texture2D(graphicsDevice, 1, 1);
             _red.SetData(new[] { Color.White });
 
-            Position = new Vector2(50, 50);
+            position = new Vector2(50, 50);
         }
 
         public void Update(GameTime gameTime)
-        {   
-            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            KeyboardState teclado = Keyboard.GetState();
+        {
 
-            float updateSpeed = speed * deltaTime;
-            
-            //Position.Y += updateSpeed; TENHO QUE FAZER UMA GRAVIDADE VALIDA
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-            if(teclado.IsKeyDown(Keys.D))
+            int screenW = graphicsDevice.PresentationParameters.BackBufferWidth;
+            int screenH = graphicsDevice.PresentationParameters.BackBufferHeight;
+
+            const int redW = 64;
+            const int redH = 64;
+
+            // aplica a gravidade
+            ApplyGravity(dt);
+            position.Y += velocity.Y * dt;
+
+            // colisão com as bordas da tela.
+            ResolveFloor(screenH, redH);
+
+            // chama o teclado
+            KeyboardCheck(dt);
+
+            position.X = MathHelper.Clamp(position.X, 0, screenW - redW);
+        }
+
+        private void ApplyGravity(float dt)
+        {
+            velocity.Y += gravity * dt;
+        }
+
+        private void ResolveFloor(int screenH, int redH)
+        {
+            isGrounded = false;
+            float floorY = screenH - redH;
+            if (position.Y >= floorY)
             {
-                Position.X += updateSpeed;
-            } 
+                position.Y = floorY;
+                velocity.Y = 0;
+                isGrounded = true;
+            }
+        }
+
+        private void KeyboardCheck(float deltaTime)
+        {
+            KeyboardState teclado = Keyboard.GetState();
+            float speed = moveSpeed * deltaTime;
+
+            if (isGrounded)
+            {
+                if (teclado.IsKeyDown(Keys.Space))
+                {
+                    velocity.Y -= 500f;
+                    isGrounded = false;
+                }
+            }
+
+            if (teclado.IsKeyDown(Keys.D))
+            {
+                position.X += speed;
+            }
             else if (teclado.IsKeyDown(Keys.A))
             {
-                Position.X -= updateSpeed;
+                position.X -= speed;
             }
         }
 
@@ -45,7 +97,7 @@ namespace Nyvorn.Source.Game.States
         {
             spriteBatch.Begin();
 
-            spriteBatch.Draw(_red, new Rectangle((int)Position.X, (int)Position.Y, 64, 64), Color.Red);
+            spriteBatch.Draw(_red, new Rectangle((int)position.X, (int)position.Y, 64, 64), Color.Red);
 
             spriteBatch.End();
         }
--- Nyvorn/obj/Debug/net8.0/Nyvorn.AssemblyInfo.cs
+++ Nyvorn/obj/Debug/net8.0/Nyvorn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nyvorn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9926ed82d7b6af0f79350fee9acfb5c284774739")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8995a334aae5cfd953f3776b0e51c6cffd8941ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nyvorn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nyvorn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
