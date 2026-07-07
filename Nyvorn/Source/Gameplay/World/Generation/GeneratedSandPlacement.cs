37d8571ef59e98a536d6fab80151c9376d224210
Israel Oliveira <israel.oliversw@gmail.com>
2026-07-07 16:11:28 -0300

    Generate desert surface sand

---
+++ Nyvorn/Source/Gameplay/World/Generation/GeneratedSandPlacement.cs
@@ -0,0 +1,8 @@
+namespace Nyvorn.Source.World.Generation
+{
+    public readonly record struct GeneratedSandPlacement(
+        int StartPixelX,
+        int StartPixelY,
+        int Width,
+        int Height);
+}
--- Nyvorn/Source/Gameplay/World/Generation/WorldGenContext.cs
+++ Nyvorn/Source/Gameplay/World/Generation/WorldGenContext.cs
@@ -19,6 +19,7 @@ namespace Nyvorn.Source.World.Generation
         public Point SpawnTile { get; set; }
         public Dictionary<string, string> DebugStats { get; } = new();
         public List<WorldGenLiquidPlacement> LiquidPlacements { get; } = new();
+        public List<GeneratedSandPlacement> SandPlacements { get; } = new();
         public TissueField TissueField { get; set; }
         public TissueGenerationResult TissueGeneration { get; set; }
         public WorldGenProgressReporter ProgressReporter { get; set; }
